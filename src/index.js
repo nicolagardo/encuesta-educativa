7c9aee5099a9ae74bb1015a42bd087b2e208ae40
nicolagardo <nicolagardo@gmail.com>
2022-06-02 09:55:32 -0300

    test44

---
+++ src/index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const passport = require('passport');
+const MySQLStore = require('express-mysql-session');
+const session = require('express-session');
+const { database } = require('./config');
+
+
+
+
+//ENV
+
+require('dotenv').config();
+
+// Intializations
+const app = express();
+// const port = require('')
+const http = require('http');
+const res = require('express/lib/response');
+const server = http.createServer(app)
+
+//TODO:  socket
+const {Server} = require('socket.io')
+const io = new Server(server)
+
+io.on('connection', socket => {
+    console.log('Un usuario se ha conectado');
+    console.log("socket: ",socket.id);
+    // socket.on('chat', msg => {
+    //     console.log(`Mensaje: ${msg}`);
+
+    // })
+//     socket.on('chat', msg => {
+//         io.emit('chat', msg)
+    // }) 
+  })
+
+
+
+require('./lib/passport');
+// Settings
+//app.set('port', process.env.PORT || 8081);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs',
+}));
+app.get('/home', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`)
+})
+app.set('view engine', '.hbs');
+
+// Middlewares
+app.use(session({
+  secret: 'miniencuestas',
+  resave: false,
+  saveUninitialized: false,
+  store: new MySQLStore(database)
+}));
+
+app.use(express.urlencoded({extended: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  app.locals.user = req.user;
+  app.locals.res = res;
+  next();
+});
+
+app.use(require('./routes/indexController'));
+app.use(require('./routes/authController'));
+app.use(require('./routes/pollController'));
+app.use(require('./routes/inscriptionsController'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+// Starting
+const port = process.env.PORT ||8080
+// const port = 8080
+// app.listen(port, () => {
+//     console.log('Server is in port', app.get('port'));
+
+//     console.log(`http://localhost:${port}`);
+//   });
+
+server.listen(port, (req, res)=> {
+  console.log('Servidor corriendo');
+  console.log(`http://localhost:${port}`);
+
+})
